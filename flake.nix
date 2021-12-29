7dfe5321f9c48159815e1acd417c809fc7873976
Max Niederman <max@maxniederman.com>
2021-12-29 07:56:25 -0800

    feat(varnish): use Sycamore & Perseus rather than Svelte & Vite

--- flake.nix
+++ flake.nix
@@ -13,14 +13,17 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        rust = with fenix.packages.${system}; complete;
+        rust = with fenix.packages.${system}; rec {
+          wasm = targets.wasm32-unknown-unknown.latest;
+          native = latest;
+          dev.toolchain = combine [ wasm.toolchain native.toolchain rust-analyzer ];
+        };
       in
       {
         devShell = pkgs.mkShell {
           nativeBuildInputs = with pkgs; [
             # Rust
-            rust.toolchain
-            rust-analyzer
+            rust.dev.toolchain
             cargo-flamegraph
             cargo-watch
             cargo-edit
@@ -31,15 +34,33 @@
             # Mold
             mold
 
-            # JavaScript
-            nodejs-16_x
-            nodePackages.pnpm
+            # Varnish
+            wasm-pack
+            (rustPlatform.buildRustPackage rec {
+              # pasted until NixOS/nixpkgs#152595 is merged
+              pname = "perseus-cli";
+              version = "0.3.0";
+
+              src = fetchCrate {
+                inherit pname version;
+                sha256 = "sha256-YyQQjuxNUxuo2PFluGyT/CpG22tgjRCfmFKA5MFRgHo=";
+              };
+
+              cargoSha256 = "sha256-SKxPsltXFH+ENexn/KDD43hGLSTgvtU9hv9Vdi2oeFA=";
+
+              meta = with lib; {
+                homepage = "https://arctic-hen7.github.io/perseus";
+                description = "A high-level web development framework for Rust with full support for server-side rendering and static generation";
+                maintainers = with maintainers; [ max-niederman ];
+                license = with licenses; [ mit ];
+              };
+            })
           ];
 
           # needed for rust-openssl
           OPENSSL_LIB_DIR = "${pkgs.openssl.out}/lib";
           OPENSSL_INCLUDE_DIR = "${pkgs.openssl.dev}/include";
-          LD_LIBRARY_PATH = nixpkgs.lib.strings.makeLibraryPath [ pkgs.openssl ];
+          LD_LIBRARY_PATH = nixpkgs.lib.strings.makeLibraryPath (with pkgs; [ openssl ]);
 
           # redirect ld calls to mold
           MOLD_PATH = "${pkgs.mold}/bin/mold";
