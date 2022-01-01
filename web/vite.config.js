96ef3a2f967886d45234a95fd54bbaecc863637d
Max Niederman <max@maxniederman.com>
2022-01-01 15:06:55 -0800

    feat(varnish): switch to Svelte w/ Vite and setup pigment-js dep

---
+++ web/vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { ViteRsw } from "vite-plugin-rsw";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    svelte(),
+    ViteRsw({
+      cli: "pnpm",
+      profile: "dev", // TODO: detect production
+      root: "../packages",
+      crates: [{
+        name: "pigment-js",
+      }],
+    })
+  ]
+});
