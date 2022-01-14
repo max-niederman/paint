f6191bfaf37be257c151db1e0b85fe9df60dbdac
Max Niederman <max@maxniederman.com>
2022-01-13 19:22:07 -0800

    feat(glaze/search): let manager read from stores

--- packages/glaze-wasm/src/search/manager.rs
+++ packages/glaze-wasm/src/search/manager.rs
@@ -1,7 +1,9 @@
 use super::Query;
 use crate::store::GlazeStore;
+use canvas::resource::*;
+use pigment::{cache, Selector, View};
+use serde::Serialize;
 use wasm_bindgen::prelude::*;
-
 use web_sys::DomException;
 
 #[wasm_bindgen]
@@ -24,15 +26,38 @@ impl Manager {
         })
     }
 
+    /// Start the query.
+    pub fn run_query(&mut self) -> Result<JsValue, JsValue> {
+        JsValue::from_serde(
+            &self
+                .stores
+                .query_view(todo!("take view as argument"), &self.query)
+                .map_err(|err| JsValue::from_str(&format!("{}", err)))?,
+        )
+        .map_err(|err| JsValue::from_str(&format!("{}", err)))
+    }
+
     /// Subscribe a callback to the query result.
     pub fn subscribe(&mut self, f: js_sys::Function) {
         self.subscribers.push(f);
+    }
+
+    /// Unsubscribe a callback from the query result.
+    pub fn unsubscribe(&mut self, f: js_sys::Function) {
+        self.subscribers.retain(|x| x != &f);
     }
 
     #[wasm_bindgen(js_name = "setQueryText")]
     pub fn set_query_text(&mut self, text: Option<String>) {
         self.query.text = text;
     }
+}
+
+#[derive(Serialize)]
+pub struct QueryResults {
+    pub courses: Vec<Course>,
+    pub assignments: Vec<Assignment>,
+    pub submissions: Vec<Submission>,
 }
 
 #[derive(Debug)]
@@ -50,4 +75,38 @@ impl Stores {
             submissions: GlazeStore::load("submissions").await?,
         })
     }
+}
+
+macro_rules! impl_stores_query_view {
+    ($( $name:ident : $ty:ty, )*) => {
+        impl Stores {
+            fn query_view<S>(&self, view: &View, selector: &S) -> cache::Result<QueryResults>
+            where
+                $( S: Selector<$ty> ),*
+            {
+                fn collect<T, E>(iter: impl Iterator<Item = Result<T, E>>) -> Result<Vec<T>, E> {
+                    let mut vec = Vec::with_capacity(iter.size_hint().0);
+                    for item in iter {
+                        vec.push(item?);
+                    }
+                    Ok(vec)
+                }
+
+                Ok(QueryResults {
+                    $(
+                        $name: collect(
+                            cache::get_all::<_, $ty>(&self.courses, view)?
+                                .map(|res| res.map(|(_, entry)| entry.resource))
+                                .filter(|res| res.is_err() || selector.matches(res.as_ref().unwrap())),
+                        )?,
+                    )*
+                })
+            }
+        }
+    };
+}
+impl_stores_query_view! {
+    courses: Course,
+    assignments: Assignment,
+    submissions: Submission,
 }
--- packages/glaze-wasm/src/search/query.rs
+++ packages/glaze-wasm/src/search/query.rs
@@ -1,7 +1,15 @@
+use canvas::Resource;
+use pigment::Selector;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-#[derive(Debug, Default, Clone, PartialEq)]
+#[derive(Debug, Default, Clone, PartialEq, Eq, Hash)]
 pub struct Query {
     pub(crate) text: Option<String>,
+}
+
+impl<R: Resource> Selector<R> for Query {
+    fn matches(&self, resource: &R) -> bool {
+        todo!("implement query matching")
+    }
 }
